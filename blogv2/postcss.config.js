5e39f6d64ce3af129d2d47d80479fc143783d2bd
nugatio <accounts@nugatio.com>
2024-11-22 05:03:23 +0100

    styling

--- blogv2/postcss.config.js
+++ blogv2/postcss.config.js
@@ -3,7 +3,6 @@ module.exports = {
       require('postcss-modules')({
         generateScopedName: '[hash:base64:5]', // Short hashed class names
         getJSON: (cssFileName, json, outputFileName) => {
-          // Save mapping between original and obfuscated class names
           const fs = require('fs');
           const path = require('path');
           const fileName = path.resolve('./class-names.json');
