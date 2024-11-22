33eee0de87af4377e02c63819b955bea9e72f490
nugatio <accounts@nugatio.com>
2024-11-22 05:24:50 +0100

    styling

--- blogv2/postcss.config.js
+++ blogv2/postcss.config.js
@@ -1,14 +1,8 @@
 module.exports = {
     plugins: [
-      require('postcss-modules')({
-        generateScopedName: '[hash:base64:5]', // Short hashed class names
-        getJSON: (cssFileName, json, outputFileName) => {
-          const fs = require('fs');
-          const path = require('path');
-          const fileName = path.resolve('./class-names.json');
-          fs.writeFileSync(fileName, JSON.stringify(json, null, 2));
-        },
+      require('autoprefixer'),
+      require('cssnano')({
+        preset: 'default',
       }),
     ],
   };
-  
